cbba9e0bf9cbd4b5658946f12b16be0e537fb909
Alen Shaju <alen.pshaju@gmail.com>
2016-11-14 11:29:33 -0600

    Assembly commit 2

--- forLoop.asm
+++ forLoop.asm
@@ -1,30 +1,30 @@
-TTL forLoop
+           TTL forLoop
 
-AREA RESET,DATA,READONLY
+           AREA RESET,DATA,READONLY
 
-EXPORT __Vectors ;
+           EXPORT __Vectors ;
 
-__VectorsDCD 0x20002000
-DCD Reset_Handler
+__Vectors  DCD 0x20002000
+           DCD Reset_Handler
 
-AREA MYCODE,CODE,READONLY
+           AREA MYCODE,CODE,READONLY
 
-ENTRY
+           ENTRY
 
-EXPORT Reset_Handler
+           EXPORT Reset_Handler
 
 Reset_Handler
-MOV32R1, #0
-ADRLR2, A
-MOV32  R0, #0
-MOV32R3, #100
+       MOV32R1, #0
+       ADR LR2, A
+       MOV32  R0, #0
+       MOV32R3, #100
 loop
-CMP R0, #100
-RSB R1, R2, R3 
-STR R1, [R2, R0, LSL #2]
-ADDR0, R0, #1
-B   loop
+       CMP R0, #100
+       RSB R1, R2, R3 
+       STR R1, [R2, R0, LSL #2]
+       ADD R0, R0, #1
+       B   loop
 
-ASPACE 400,1
+A      SPACE 400,1
 
-END
+       END
